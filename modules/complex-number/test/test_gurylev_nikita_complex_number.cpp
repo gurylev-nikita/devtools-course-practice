0b020520abf493a73715d6e180d4af9424fdc1d0
gurylev-nikita <gurylev.nickita@yandex.ru>
2021-03-31 19:44:48 +0300

    fix1

--- modules/complex-number/test/test_gurylev_nikita_complex_number.cpp
+++ modules/complex-number/test/test_gurylev_nikita_complex_number.cpp
@@ -1,14 +1,20 @@
 // Copyright 2021 Gurylev Nikita
 
 #include <gtest/gtest.h>
+
 #include <cmath>
+
+#include <tuple>
+
 #include "include/complex_number.h"
 
 TEST(Gurylev_Nikita_ComplexNumberTest, EqualityCheck) {
     double re = 1.0;
     double im = 2.0;
+
     ComplexNumber z(re, im);
     ComplexNumber result = (z * z) / z;
+
     ASSERT_DOUBLE_EQ(1.0, result.getRe());
     ASSERT_DOUBLE_EQ(2.0, result.getIm());
 }
@@ -16,19 +22,29 @@ TEST(Gurylev_Nikita_ComplexNumberTest, EqualityCheck) {
 TEST(Gurylev_Nikita_ComplexNumberTest, ArithmeticOperation) {
     ComplexNumber z(8.0, 9.0);
     ComplexNumber q(4.0, 3.0);
+
     ComplexNumber num1 = (z + q) * (z - q);
+    ComplexNumber num2 = (z + q) * (z - q);
     ComplexNumber result(-24.0, 120.0);
+
     ASSERT_EQ(result, num1);
 }
 
-TEST(Gurylev_Nikita_ComplexNumberTest, ComputationComparison) {
-    ComplexNumber z(5.0, 10.0);
-    ComplexNumber q(3.0, 5.0);
-    ComplexNumber g(2.0, 3.0);
-    ComplexNumber result1 = z * q + g;
-    ComplexNumber num1(z.getRe(), z.getIm());
-    ComplexNumber num2(q.getRe(), q.getIm());
-    ComplexNumber num3(g.getRe(), g.getIm());
-    ComplexNumber result2 = num1 * num2 + num3;
-    ASSERT_EQ(result1, result2);
+typedef testing::TestWithParam<std::tuple<double, double, double, double>>
+Gurylev_Nikita_ComplexNumberTestTest_Param;
+TEST_P(Gurylev_Nikita_ComplexNumberTestTest_Param, ÑomparisonOperation) {
+    ComplexNumber num1(std::get<0>(GetParam()), std::get<1>(GetParam()));
+    ComplexNumber num2(std::get<2>(GetParam()), std::get<3>(GetParam()));
+
+    ComplexNumber result1 = ((num2 * num1) / num1); 
+    ComplexNumber result2 = ((result1 / num2) * num1);
+
+    ASSERT_EQ(result2, num1);
 }
+
+INSTANTIATE_TEST_CASE_P(/**/, Gurylev_Nikita_ComplexNumberTestTest_Param,
+    testing::Values(
+        std::make_tuple(2.0, 6.0, 6.0, 15.0),
+        std::make_tuple(2.0, 4.0, 0.0, 5.0),
+        std::make_tuple(2.0, 3.0, 15.0, 7.0),
+        std::make_tuple(10.0, 5.0, 3.0, 8.0)));
