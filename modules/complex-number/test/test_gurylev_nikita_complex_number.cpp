afdbf9c28f381c74c3b677e489febbaf4703b3d3
gurylev-nikita <gurylev.nickita@yandex.ru>
2021-04-01 15:04:40 +0300

    fix6

--- modules/complex-number/test/test_gurylev_nikita_complex_number.cpp
+++ modules/complex-number/test/test_gurylev_nikita_complex_number.cpp
@@ -14,8 +14,8 @@ TEST(Gurylev_Nikita_ComplexNumberTest, EqualityCheck) {
     ComplexNumber z(re, im);
     ComplexNumber result = (z * z) / z;
 
-    ASSERT_DOUBLE_EQ(1.0, result.getRe());
-    ASSERT_DOUBLE_EQ(2.0, result.getIm());
+    ASSERT_DOUBLE_EQ(re, result.getRe());
+    ASSERT_DOUBLE_EQ(im, result.getIm());
 }
 
 TEST(Gurylev_Nikita_ComplexNumberTest, ArithmeticOperation) {
@@ -25,24 +25,24 @@ TEST(Gurylev_Nikita_ComplexNumberTest, ArithmeticOperation) {
     ComplexNumber num1 = (z + q) * (z - q);
     ComplexNumber result(-24.0, 120.0);
 
-    ASSERT_EQ(result, num1);
+    ASSERT_EQ(num1, result);
 }
 
 typedef testing::TestWithParam<std::tuple<double, double, double, double>>
 Gurylev_Nikita_ComplexNumberTest_Param;
 TEST_P(Gurylev_Nikita_ComplexNumberTest_Param, ComparisonOperation) {
-    ComplexNumber num1(std::get<0>(GetParam()), std::get<1>(GetParam()));
-    ComplexNumber num2(std::get<2>(GetParam()), std::get<3>(GetParam()));
+    ComplexNumber z(std::get<0>(GetParam()), std::get<1>(GetParam()));
+    ComplexNumber q(std::get<2>(GetParam()), std::get<3>(GetParam()));
 
-    ComplexNumber result1 = ((num2 * num1) / num1);
-    ComplexNumber result2 = ((result1 / num2) * num1);
+    ComplexNumber result1 = (z * z * z) + (q * q * q);
+    ComplexNumber result2 = (z + q) * (z * z - z * q + q * q);
 
-    ASSERT_EQ(result2, num1);
+    ASSERT_EQ(result1, result2);
 }
 
 INSTANTIATE_TEST_CASE_P(/**/, Gurylev_Nikita_ComplexNumberTest_Param,
-    testing::Values(
-        std::make_tuple(2.0, 6.0, 6.0, 15.0),
-        std::make_tuple(2.0, 4.0, 0.0, 5.0),
-        std::make_tuple(2.0, 3.0, 15.0, 7.0),
-        std::make_tuple(10.0, 5.0, 3.0, 8.0)));
+    ::testing::Values(
+        std::make_tuple(2.0, 3.0, 5.7, 1.5),
+        std::make_tuple(6.0, 2.0, 4.0, 5.0),
+        std::make_tuple(4.5, 7.0, 9.0, 3.0),
+        std::make_tuple(3.0, 3.0, 4.0, 4.0)));
