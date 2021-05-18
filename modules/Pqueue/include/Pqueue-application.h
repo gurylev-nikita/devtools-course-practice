d804596e1e5937cb1b2b55d0907f7a63875c922f
gurylev-nikita <gurylev.nickita@yandex.ru>
2021-05-18 12:47:19 +0300

    done

---
+++ modules/Pqueue/include/Pqueue-application.h
@@ -0,0 +1,20 @@
+// Copyright 2021 Gurylev Nikita
+#ifndef MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_APPLICATION_H_
+#define MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_APPLICATION_H_
+
+#include <string>
+#include "include/Pqueue.h"
+
+class PriorityApp {
+ private:
+    PQueue q;
+    std::string message_;
+    void Help(const char* appname, const char* message = "");
+    bool ValidateNumberOfArguments(int argc, const char** argv);
+    int ParseOperation(const char** s);
+ public:
+    PriorityApp() = default;
+    std::string operator()(int argc, const char** argv);
+};
+
+#endif  // MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_APPLICATION_H_
